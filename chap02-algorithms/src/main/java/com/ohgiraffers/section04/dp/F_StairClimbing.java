be48df8d6271ea47b6683f79466f1d291cbb7840
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-11 14:47:04 +0900

    commit

--- chap02-algorithms/src/main/java/com/ohgiraffers/section04/dp/F_StairClimbing.java
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section04/dp/F_StairClimbing.java
@@ -9,8 +9,26 @@ public class F_StairClimbing {
     public static int solution(String input) throws IOException {
         BufferedReader br = new BufferedReader(new StringReader(input));
         int n = Integer.parseInt(br.readLine());        // 계단의 개수
+        int[] arr = new int[n + 1];                     // 계단별 점수
+        int[] dp = new int[n + 1];                      // 최적(최대 점수 합계)의 값 누적
 
+        for(int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-        return 0;
+        // 초기값 설정
+        dp[1] = arr[1];
+        if(n >= 2) dp[2] = dp[1] + arr[2];
+
+        for(int i = 3; i <= n; i++) {
+            /*
+            * 가능 상황 1. 직전계단에서 올라오는 상황
+            * -> 연달아 밟지 않아야 하므로 i - 3 -> i - 1 -> i
+            * 가능 상황 2. 두 계단 전에서 올라오는 상황 -> i - 2전의 상황을 고려할 필요 x
+            * 1번 : dp[i-3] + arr[i-1] vs 2번: dp[i-2] + arr[i]
+            * */
+            dp[i] = Math.max(dp[i-3]+arr[i-1], dp[i-2]) + arr[i];
+        }
+        return dp[n];
     }
 }
